d379a696fcfe2bc48b51e847426ab4e9e7db2291
IshLeFish <88610959+IshLeFish@users.noreply.github.com>
2025-01-13 23:42:34 +1100

    I2C confirmed
    playing with example scripts and various libraries to implement sensor fusion / digital motion processing

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_MPU6050.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_MPU6050 mpu;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+int i = 0;
+
+void setup()
+{
+    delay(500);
+    Serial.begin(9600);
+    if (!mpu.begin())
+    {
+        Serial.println("Sensor init failed");
+        while (1)
+            yield();
+    }
+    Serial.println("hello world");
+    mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_94_HZ);
+    
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+float angle = 0;
+float gyroX = 0;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    gyroX = g.gyro.x - 0.02248;
+    angle = gyroX+angle;
+    //Serial.print("Rotation X: ");
+    Serial.print(gyroX);
+    Serial.print('\t');
+    Serial.println(angle);
+    delay(50);
+    
 }
