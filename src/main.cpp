233bbd2b9f55f76100a7a9da7cf8fa0846d46890
IshLeFish <88610959+IshLeFish@users.noreply.github.com>
2025-01-14 14:56:11 +1100

    gyro accuracy optimisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+
+
+
 #include <Wire.h>
 #include <Adafruit_BusIO_Register.h>
 #include <Adafruit_Sensor.h>
@@ -12,7 +15,8 @@ int i = 0;
 void setup()
 {
     delay(500);
-    Serial.begin(9600);
+    pinMode(LED_BUILTIN,OUTPUT);
+    Serial.begin(115200);
     if (!mpu.begin())
     {
         Serial.println("Sensor init failed");
@@ -27,17 +31,72 @@ void setup()
 
 float angle = 0;
 float gyroX = 0;
+float prevgyroX = 0;
+int state = 1;
+unsigned long period = 0;
+
+
 
 void loop()
 {
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
-    gyroX = g.gyro.x - 0.02248;
-    angle = gyroX+angle;
+    
+    // static error offset
+    gyroX = g.gyro.x - 0.02248; 
+    
+    //Dynamic error
+    if (abs(gyroX) <0.1) {  
+        digitalWrite(LED_BUILTIN,HIGH);
+    } else {
+        digitalWrite(LED_BUILTIN,LOW);
+    }
+
+    // steady state reset
+    if(abs(gyroX)>=0.01){ 
+        angle = gyroX+angle;
+    }
+    if (state != 2 && gyroX == prevgyroX) {
+        period = millis();
+        state = 2;
+    }
+    if (state == 2 && millis() - period >=200) {
+        angle = 0;
+        state = 1;
+    }
+
+    //display
+
     //Serial.print("Rotation X: ");
     Serial.print(gyroX);
     Serial.print('\t');
+    Serial.print(state);
+    Serial.print('\t');
     Serial.println(angle);
+
+    
+
     delay(50);
+    prevgyroX = gyroX;
     
 }
+
+
+/*
+bool i = 0;
+
+
+
+void setup() {
+    pinMode(LED_BUILTIN,OUTPUT);
+    Serial.begin(115200);
+}
+
+void loop() {
+    digitalWrite(LED_BUILTIN,i);
+    Serial.println(i);
+    i = !i;
+    delay(5*(sqrt(millis())));
+}
+
+*/
